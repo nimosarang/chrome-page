213aeecf8f9466f3d1c3acfeb396865234023003
nimosarang <111734043+nimosarang@users.noreply.github.com>
2024-03-17 22:49:01 +0900

    북마크 추가 버튼 세팅

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,1 +1,11 @@
 const newBookMarkForm = document.getElementById("bookmark-item-input-form");
+
+let bookmarkList = [];
+if (localStorage.getItem("bookmarkList")) {
+  bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
+} else {
+  localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+}
+
+let isAddBtnClick = false;
+newBookMarkForm.style.display = "none";
